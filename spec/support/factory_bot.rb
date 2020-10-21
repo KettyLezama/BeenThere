1fbea5f65bcf09345ee068cc21b6206b6d5982cc
RobPerez155 <robert.perez.psirho@gmail.com>
2020-10-21 11:23:14 -0400

    Factory bot and rspec, one error

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -2,9 +2,12 @@ require 'factory_bot'
 
 FactoryBot.define do
   factory :user do
-    sequence(:email) {|n| "user#{n}@example.com" }
+    sequence(:email) { |n| "user#{n}@example.com" }
     password { 'password' }
     password_confirmation { 'password' }
+    first_name { 'John' }
+    last_name { 'Doe' }
+    sequence(:username) { |n| "user#{n}" }
   end
 
 end
